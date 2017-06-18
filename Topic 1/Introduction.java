7b36e255240ab0856de2051b5ef669cdd5e03dc5
HPENVY1\OWNER <psalm46.5@hotmail.com>
2017-06-18 15:11:33 -0700

    My first assignment. I did not find any mention of flowcharts until I searched chapter 3 in the book.

---
+++ Topic 1/Introduction.java
@@ -0,0 +1,11 @@
+/** This is my first program in Java.
+ * This program introduces myself.
+ * This program demonstrates creating a project and a class.
+ */
+public class Introduction {
+    public static void main(String[] args){
+        System.out.println("My name is Elijah.");
+        System.out.println("My favorite color is red.");
+        System.out.println("If I won the lottery, I would buy a plane.");         
+    }
+}
